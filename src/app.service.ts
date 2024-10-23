92477097dc973132d818ceb08020c4dc2abf7bca
Aryan Agarwal <aryankush025@gmail.com>
2024-10-23 17:14:25 +0530

    Add multi-service support and configuration handling
    Refactored the application to support multiple AI services: OpenAI, Anthropic, and Mistral.
    Introduced @nestjs/config for environment configuration management to streamline service API key set up.
    Updated the `.env.example` and integrated query parameters into the API for dynamic service selection.
    Each service invocation now handles separate key usage and message processing, improving flexibility and maintainability.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,71 @@
 import { Injectable } from '@nestjs/common';
+import { ChatOpenAI } from '@langchain/openai';
+import { ChatAnthropic } from '@langchain/anthropic';
+import { ChatMistralAI } from '@langchain/mistralai';
+import { HumanMessage, SystemMessage } from '@langchain/core/messages';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(service: 'openai' | 'anthropic' | 'mistral') {
+    if (service === 'openai') {
+      return this.openai();
+    }
+    if (service === 'anthropic') {
+      return this.anthropic();
+    }
+    if (service === 'mistral') {
+      return this.mistral();
+    }
+
+    return { service: 'unknown', res: 'unknown' };
+  }
+
+  private async openai() {
+    const model = new ChatOpenAI({
+      model: 'gpt-4',
+      apiKey: process.env.OPENAI_API_KEY_1,
+    });
+    const messages = [
+      new SystemMessage('Translate the following from English into Italian'),
+      new HumanMessage('hi!'),
+    ];
+
+    const res = await model.invoke(messages);
+
+    return { service: 'openai', res };
+  }
+
+  private async anthropic() {
+    const model = new ChatAnthropic({
+      model: 'claude-3-5-sonnet-20240620',
+      temperature: 0,
+      apiKey: process.env.ANTHROPIC_API_KEY_1,
+    });
+
+    const messages = [
+      new SystemMessage('Translate the following from English into Italian'),
+      new HumanMessage('hi!'),
+    ];
+
+    const res = await model.invoke(messages);
+
+    return { service: 'anthropic', res };
+  }
+
+  private async mistral() {
+    const model = new ChatMistralAI({
+      model: 'mistral-large-latest',
+      temperature: 0,
+      apiKey: process.env.MISTRAL_API_KEY_1,
+    });
+
+    const messages = [
+      new SystemMessage('Translate the following from English into Italian'),
+      new HumanMessage('hi!'),
+    ];
+
+    const res = await model.invoke(messages);
+
+    return { service: 'mistral', res };
   }
 }
